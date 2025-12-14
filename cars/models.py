f154fa8e6c7adf3d72aac5ded69a06cf663c9776
Ramon Brito <ramon.mecatec@gmail.com>
2025-12-14 12:21:13 -0300

    feat: Refactor `Car.brand` from `CharField` to `ForeignKey` to a new `Brand` model with data migration, and update `.gitignore`.

--- cars/models.py
+++ cars/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
+
+
+
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=200)
-    brand = models.CharField(max_length=200)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='cars', null=True, blank=True)
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
     price = models.FloatField(blank=True, null=True)
