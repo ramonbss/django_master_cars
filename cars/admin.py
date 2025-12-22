6f5917d39332f9fac9034456fe88a9f6f07ad9da
Ramon Brito <ramon.mecatec@gmail.com>
2025-12-22 10:15:54 -0300

    feat: Register Brand model in Django admin with its custom BrandAdmin class.

--- cars/admin.py
+++ cars/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Car
+from .models import Car, Brand
 
 class CarAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'price')
     search_fields = ('model', 'brand')
 
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
 admin.site.register(Car, CarAdmin)
+admin.site.register(Brand, BrandAdmin)
 
 
