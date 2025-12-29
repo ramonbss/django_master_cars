e97b03d19b7d29f338b481ec97bdcf98d6806c76
Ramon Brito <ramon.mecatec@gmail.com>
2025-12-29 09:51:03 -0300

    feat: Add car listing page with view, template and url settings

--- app/urls.py
+++ app/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from cars import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cars/', views.cars, name='cars'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Car
 
 # Create your views here.
+def cars(request):
+    cars = Car.objects.all()
+    context = {
+        'cars': cars
+    }
+    return render(request, 'cars.html', context)
