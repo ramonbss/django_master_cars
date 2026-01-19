2ad72c2d5054ebfc8c5221dc18df253c2a7fe680
smtpsai <smtpsai@gmail.com>
2026-01-19 18:03:45 +0000

    feat: Update Django settings to configure Allowed_Hosts and to support github Codespace

--- app/settings.py
+++ app/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -26,11 +25,18 @@ SECRET_KEY = 'django-insecure-l!@iyn88^@@tbsk0hzo(+a3fsitv%z(=&r_%+p7q1kq6%^!ub)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['127.0.0.1','localhost', f'{CODESPACE_NAME}-8000.app.github.dev']
+TRUSTED_ORIGINS = [f'https://{CODESPACE_NAME}-8000.app.github.dev', 
+                            'https://localhost:8000',
+                            'http://localhost:8000',]
+ALLOWED_HOSTS.extend(TRUSTED_ORIGINS)
+if CODESPACE_NAME:
+    print(f'Running in Codespace: {CODESPACE_NAME}')
+    CSRF_TRUSTED_ORIGINS = TRUSTED_ORIGINS.copy()
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
